6754e591e15ad220bcb2a2c0d6b32f91d5395434
gcosentino1 <85801475+joeycose@users.noreply.github.com>
2023-02-22 15:29:04 -0500

    Added hover effect to nav.links

--- my-app/components/NavbarMain.js
+++ my-app/components/NavbarMain.js
@@ -1,18 +1,35 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
 
 export default function MainNav() {
+    const [hovered, setHovered] = useState(false);
+
+    const handleMouseOver = () => {
+        setHovered(true);
+    }
+
+    const handleMouseOut = () => {
+        setHovered(false);
+    }
+
+    const linkStyle = {
+        color: hovered ? "white" : "grey",
+        paddingRight: '20px',
+        cursor: 'pointer'
+    };
+
     return (
-        <Navbar id="NavbarMain" collapseOnSelect expand="lg" variant="dark" className="" style={{borderBottom: '1px solid grey'}}>
-            <Container className="align-items-centre">
+        <Navbar id="NavbarMain" collapseOnSelect expand="lg" variant="dark" className="" style={{ borderBottom: '1px solid grey' }}>
+            <Container className="align-items-center">
                 <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mx-auto" style={{ fontSize: '17px' }}>
-                        <Nav.Link style={{ color: 'grey', paddingRight: '20px' }} href="/">Home</Nav.Link>
-                        <Nav.Link style={{ color: 'grey', paddingRight: '20px' }} href="/projects">Projects</Nav.Link>
-                        <Nav.Link style={{ color: 'grey', paddingRight: '20px' }} href="/contact">Contact</Nav.Link>
-                        <Nav.Link style={{ color: 'grey', paddingRight: '20px' }} href="/blog">Blog</Nav.Link>
+                        <Nav.Link style={linkStyle} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} href="/">Home</Nav.Link>
+                        <Nav.Link style={linkStyle} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} href="/projects">Projects</Nav.Link>
+                        <Nav.Link style={linkStyle} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} href="/contact">Contact</Nav.Link>
+                        <Nav.Link style={linkStyle} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} href="/blog">Blog</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
